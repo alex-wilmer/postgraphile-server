4136e2ebc810a7d1b8a9a8d1a8cb1cc11385ec2c
Alex Wilmer <djazium@gmail.com>
2019-10-01 18:35:09 -0400

    oops multiple returns

--- src/migrations/20191001151031_initialize.js
+++ src/migrations/20191001151031_initialize.js
@@ -1,15 +1,16 @@
-let UserAccount = require('../models/UserAccount')
+let UserAccount = require("../models/UserAccount");
 
 // TODO: omit comment for knex_migrations and knex_migrations_lock
 // comment on table knex_migrations is E'@omit';
 // comment on table knex_migrations_lock is E'@omit';
 
 exports.up = knex => {
-  return UserAccount(knex)
+  return UserAccount(knex);
 };
 
 exports.down = knex => {
-  return knex.schema.dropTable("UserAccount");
-  return knex.schema.dropTable("AccountSettings");
-  return knex.schema.dropTable("NotificationPreferences");
+  return knex.schema
+    .dropTable("UserAccount")
+    .dropTable("AccountSettings")
+    .dropTable("NotificationPreferences");
 };
